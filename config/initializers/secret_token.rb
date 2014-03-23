ff92d958253fbb0f9b0ecde5a2f9afde586a1d0b
Speedyx <cacope76@hotmail.com>
2014-03-23 17:00:19 +0100

    CODIGO

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Citas::Application.config.secret_key_base = '11175bbb986fe491bc504025b49269bc0d40e485b609132c7f1aa7a8ab6f5f67f10c68a569b96b93fde6bccd7e6562cc1c8da7742a8261d6046a297d7510013a'
